f427ed9bff030335e95404df0b67b718c117eea5
Maciel Custódio <maciel_custodio@icloud.com>
2024-06-02 19:17:14 -0300

    feat: :sparkles: aprendendo sobre construtores

---
+++ Java/01 - Fundamentos/HelloWorld/src/construtor/Construtor.java
@@ -0,0 +1,21 @@
+package construtor;
+
+public class Construtor {
+  private int numero;
+
+  public Construtor(int numero) {
+    this.numero = numero;
+  }
+
+  public Construtor(int numero, String tetxo) {
+    this.numero = numero;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+}

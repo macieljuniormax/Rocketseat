3271862fcc2a807a40dd144ff85a82999aa415e3
Maciel Custódio <maciel_custodio@icloud.com>
2024-06-03 20:21:00 -0300

    feat: :tada: criando primeiro projeto spring boot

---
+++ Java/01 - Fundamentos/primeiro-projeto-springboot/src/main/java/br/com/macielcustodio/primeiro_projeto_springboot/PrimeiroProjetoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.macielcustodio.primeiro_projeto_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringbootApplication.class, args);
+	}
+
+}

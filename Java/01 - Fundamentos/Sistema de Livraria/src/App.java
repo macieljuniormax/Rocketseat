c9062ea408b7fc51bef53639315b955ce16dd32d
macieljuniormax <maciel_custodio@icloud.com>
2025-04-02 20:43:41 -0300

    feat: :sparkles: cria função para realizar a escrita em arquivos

--- Java/01 - Fundamentos/Sistema de Livraria/src/App.java
+++ Java/01 - Fundamentos/Sistema de Livraria/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        ArquivoUtil.escreverArquivo(TipoArquivo.LIVROS, "Fazendo um teste");
+        ArquivoUtil.escreverArquivo(TipoArquivo.LIVROS, "Fazendo um teste");
     }
 }
--- Java/01 - Fundamentos/Sistema de Livraria/src/ArquivoUtil.java
+++ Java/01 - Fundamentos/Sistema de Livraria/src/ArquivoUtil.java
@@ -1,20 +1,21 @@
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class ArquivoUtil {
     public static void escreverArquivo(TipoArquivo tipoArquivo, String conteudo) {
         try {
             Path directory = Paths.get("src", "resources");
-            Path filePath = directory.resolve(tipoArquivo.getNomeArquivo() + "txt");
+            Path filePath = directory.resolve(tipoArquivo.getNomeArquivo() + ".txt");
 
             if (!Files.exists(directory)) {
                 Files.createDirectories(directory);
             }
 
-            if (!Files.exists(filePath)) {
-                Files.createFile(filePath);
-            }
+            Files.write(filePath, conteudo.toString().getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND,
+                    StandardOpenOption.CREATE);
 
         } catch (Exception e) {
             e.printStackTrace();

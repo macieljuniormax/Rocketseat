388e5f654f715581272c5d55b9d3be0c4f1fe20c
macieljuniormax <maciel_custodio@icloud.com>
2025-06-02 20:56:57 -0300

    feat: :sparkles: integrate Navbar component and update app structure

--- Angular/01 - Fundamentos/gerador-certificado/src/app/app.ts
+++ Angular/01 - Fundamentos/gerador-certificado/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'gerador-certificado';
+  protected navbar: boolean = true;
 }

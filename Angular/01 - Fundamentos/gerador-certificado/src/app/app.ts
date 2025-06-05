6eae64316aff3309f0ee7f8ac754db3cba8a9a6f
macieljuniormax <maciel_custodio@icloud.com>
2025-06-05 19:57:44 -0300

    feat: :sparkles: create Certificados component with template, styles, and tests; update app structure to include new component

--- Angular/01 - Fundamentos/gerador-certificado/src/app/app.ts
+++ Angular/01 - Fundamentos/gerador-certificado/src/app/app.ts
@@ -5,10 +5,11 @@ import { PrimaryButton } from './components/primary-button/primary-button';
 import { SecondaryButton } from './components/secondary-button/secondary-button';
 import { ItemCertificado } from "./components/item-certificado/item-certificado";
 import { BaseUi } from "./components/base-ui/base-ui";
+import { Certificados } from "./pages/certificados/certificados";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi],
+  imports: [Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

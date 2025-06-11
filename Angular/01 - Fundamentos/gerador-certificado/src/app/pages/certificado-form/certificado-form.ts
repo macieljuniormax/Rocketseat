e0e547f26bbe0dfd071bd4482912858f2c3b59ad
macieljuniormax <maciel_custodio@icloud.com>
2025-06-10 21:38:30 -0300

    feat: :sparkles: enhance certificado form with validation and data binding for nome and atividade fields

--- Angular/01 - Fundamentos/gerador-certificado/src/app/pages/certificado-form/certificado-form.ts
+++ Angular/01 - Fundamentos/gerador-certificado/src/app/pages/certificado-form/certificado-form.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { PrimaryButton } from "../../components/primary-button/primary-button";
 import { SecondaryButton } from "../../components/secondary-button/secondary-button";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [PrimaryButton, SecondaryButton],
+  imports: [FormsModule, PrimaryButton, SecondaryButton],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css'
 })
 export class CertificadoForm {
+  private _nome: string = '';
+  private _atividade: string = '';
+  private _atividades: Array<string> = ['Atividade 1', 'Atividade 2', 'Atividade 3'];
 
+  // Getters and Setters
+  public get nome(): string { return this._nome; }
+  public set nome(value: string) { this._nome = value; }
+  public get atividade(): string { return this._atividade; }
+  public set atividade(value: string) { this._atividade = value; }
+  public get atividades(): Array<string> { return this._atividades; }
+  public set atividades(value: Array<string>) { this._atividades = value; }
 }

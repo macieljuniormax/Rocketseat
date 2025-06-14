7574b3f7c6f96535ef5441c27d831a6f06152065
macieljuniormax <maciel_custodio@icloud.com>
2025-06-14 19:41:07 -0300

    feat: :sparkles: refactor CadastroRepository to rename connection variable and implement incluir method for adding records

--- Java/02 - Java Collections, Maven e JDBC/02 - Maven e JDBC/maven-jdbc/src/main/java/app/CadastroRepository.java
+++ Java/02 - Java Collections, Maven e JDBC/02 - Maven e JDBC/maven-jdbc/src/main/java/app/CadastroRepository.java
@@ -1,16 +1,28 @@
 package app;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class CadastroRepository {
-  private Connection conexao;
+  private Connection connection;
 
   public CadastroRepository() {
-    this.conexao = FabricaConexao.getConexao();
+    this.connection = FabricaConexao.getConexao();
   }
 
-  public void salvar(Cadastro cadastro) {
+  public void incluir(Cadastro cadastro) {
+    try {
+      String instrucaoSQL = "INSERT INTO \"public\".\"tab_cadastro\" (nome, idade) VALUES(?, ?);";
+      PreparedStatement statement = connection.prepareStatement(instrucaoSQL);
+      statement.setString(1, cadastro.getNome());
+      statement.setInt(2, cadastro.getIdade());
+      statement.executeUpdate();
+      System.out.println("Record created.");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   };
 
   public void alterar(Cadastro cadastro) {

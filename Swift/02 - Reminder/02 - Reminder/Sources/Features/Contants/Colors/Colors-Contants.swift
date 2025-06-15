845476156dac0d13095b94937b1eda778c6c42c6
macieljuniormax <maciel_custodio@icloud.com>
2025-06-15 15:57:23 -0300

    feat: Add Colors struct for color constants in the project

---
+++ Swift/02 - Reminder/02 - Reminder/Sources/Features/Contants/Colors/Colors-Contants.swift
@@ -0,0 +1,26 @@
+//
+//  Colors-Contants.swift
+//  02 - Reminder
+//
+//  Created by Maciel Ferreira Custódio Júnior on 15/06/25.
+//
+
+import Foundation
+import UIKit
+
+public struct Colors {
+    static let red_primary = UIColor(red: 192/255, green: 38/255, blue: 54/255, alpha: 1)
+    static let red_secondary = UIColor(red: 235/255, green: 60/255, blue: 77/255, alpha: 1)
+    
+    static let blue_primary = UIColor(red: 51/255, green: 79/255, blue: 220/255, alpha: 1)
+    static let blue_secondary = UIColor(red: 110/255, green: 131/255, blue: 236/255, alpha: 1)
+    
+    static let gray_100 = CIColor(red: 248/255, green: 250/255, blue: 251/255, alpha: 1)
+    static let gray_200 = CIColor(red: 232/255, green: 238/255, blue: 243/255, alpha: 1)
+    static let gray_300 = CIColor(red: 215/255, green: 225/255, blue: 234/255, alpha: 1)
+    static let gray_400 = CIColor(red: 202/255, green: 215/255, blue: 226/255, alpha: 1)
+    static let gray_500 = CIColor(red: 162/255, green: 185/255, blue: 205/255, alpha: 1)
+    static let gray_600 = CIColor(red: 77/255, green: 112/255, blue: 143/255, alpha: 1)
+    static let gray_700 = CIColor(red: 41/255, green: 60/255, blue: 76/255, alpha: 1)
+    static let gray_800 = CIColor(red: 23/255, green: 34/255, blue: 43/255, alpha: 1)
+}

2c89596a4166dba4e23be8f1078fa5872dc5c5fa
macieljuniormax <maciel_custodio@icloud.com>
2025-06-16 17:23:32 -0300

    feat: Integrate Firebase authentication in LoginBottomSheetViewModel and add GoogleService-Info.plist

--- Swift/02 - Primeiro Projeto/Reminder/Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
+++ Swift/02 - Primeiro Projeto/Reminder/Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -6,10 +6,18 @@
 //
 
 import Foundation
+import Firebase
 
 class LoginBottomSheetViewModel {
     func doAuthentication(username: String, password: String) {
         print(username, password)
-        
+        Firebase.Auth.auth().signIn(withEmail: username, password: password) { [weak self] authResult, error in
+            if let error = error {
+                print("Autenticação não foi realizada com sucesso \(error)")
+                
+            } else {
+                print(authResult ?? "")
+            }
+        }
     }
 }

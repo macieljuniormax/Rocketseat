ff77c5b59450ba5595035d64e94a2d15e4deb1c4
macieljuniormax <maciel_custodio@icloud.com>
2025-06-18 21:13:28 -0300

    refactor: Clean up whitespace in LoginViewController for improved readability

--- Swift/02 - Primeiro Projeto/Reminder/Reminder/Features/Login/Presentation/LoginViewController.swift
+++ Swift/02 - Primeiro Projeto/Reminder/Reminder/Features/Login/Presentation/LoginViewController.swift
@@ -13,32 +13,32 @@ class LoginViewController: UIViewController {
     private let loginViewModel: LoginViewModel = LoginViewModel()
     private var handleAreaHeigh: CGFloat = 50.0
     private weak var flowDelegate: LoginFlowDelegate?
-    
+
     init(contentView: LoginView,
          flowDelegate: LoginFlowDelegate) {
         self.loginView = contentView
         self.flowDelegate = flowDelegate
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() -> Void {
         super.viewDidLoad()
-        
+
         self.setupUI()
         self.setupBindViewModel()
     }
-    
+
     private func setupUI() -> Void {
         self.view.addSubview(self.loginView)
-        
+
         self.loginView.viewDelegate = self
         self.buildHierarchy()
     }
-    
+
     private func buildHierarchy() -> Void {
         self.loginView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -46,26 +46,26 @@ class LoginViewController: UIViewController {
             self.loginView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
             self.loginView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
         ])
-        
+
         let heightConstraint = self.loginView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.5)
         heightConstraint.isActive = true
     }
 
-    
+
     private func handlePanGesture() -> Void {
-        
+
     }
-    
+
     private func setupBindViewModel() -> Void {
         self.loginViewModel.onLoginSucess = { [weak self] username in
             self?.presentSaveLoginAlert(email: username)
         }
-        
+
         self.loginViewModel.onLoginError = { [weak self] errorMessage in
             self?.presentSaveLoginAlert(message: errorMessage)
         }
     }
-    
+
     private func presentSaveLoginAlert(email: String) -> Void {
         let alertController: UIAlertController = UIAlertController(title: "Salvar Acesso",
                                                                    message: "Deseja se manter conectado?",
@@ -76,18 +76,18 @@ class LoginViewController: UIViewController {
             UserDefaultsManager.saveUser(user: user)
             self.flowDelegate?.navigateToHome()
         }
-        
+
         let cancelAction: UIAlertAction = UIAlertAction(title: "Não",
                                                       style: UIAlertAction.Style.cancel) { _ in
             self.flowDelegate?.navigateToHome()
         }
-        
+
         alertController.addAction(saveAction)
         alertController.addAction(cancelAction)
-        
+
         self.present(alertController, animated: true)
     }
-    
+
     private func presentSaveLoginAlert(message: String) -> Void {
         let alertController: UIAlertController = UIAlertController(title: "Credenciais Inválidas",
                                                                    message: message,
@@ -95,7 +95,7 @@ class LoginViewController: UIViewController {
         let retryAction: UIAlertAction = UIAlertAction(title: "Tentar Novamente",
                                                         style: UIAlertAction.Style.default)
         alertController.addAction(retryAction)
-        
+
         self.present(alertController, animated: true)
     }
 }

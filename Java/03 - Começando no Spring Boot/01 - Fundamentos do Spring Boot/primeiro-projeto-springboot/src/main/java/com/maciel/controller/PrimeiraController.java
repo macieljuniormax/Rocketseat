3136bfb36c54f340b7d1c1310c6db2228fc069cb
macieljuniormax <maciel_custodio@icloud.com>
2025-07-06 19:20:02 -0300

    feat: :sparkles: Implement PrimeiraController with multiple REST endpoints for various request types

---
+++ Java/03 - Começando no Spring Boot/01 - Fundamentos do Spring Boot/primeiro-projeto-springboot/src/main/java/com/maciel/controller/PrimeiraController.java
@@ -0,0 +1,51 @@
+package com.maciel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+
+
+@RestController
+@RequestMapping("/primeiraController")
+public class PrimeiraController {
+  @GetMapping("/primeiroMetodo/{id}")
+  public String primeiroMetodo(@PathVariable String id) {
+      return "O parâmetro é : " + id;
+  }
+
+  @GetMapping("/metodoComQueryParams")
+  public String metodoComQueryParams(@RequestParam String id) {
+      return "O parâmetro com metodoComQueryParams é : " + id;
+  }
+
+  @GetMapping("/metodoComQueryParams2")
+  public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
+      return "O parâmetro com metodoComQueryParams é : " + allParams.entrySet();
+  }
+
+  @PostMapping("/metodoComBodyParams")
+  public String metodoComBodyParams(@RequestBody Usuario usuario) {
+      return "metodoComBodyParams " + usuario.username;
+  }
+
+  @PostMapping("/metodoComHeaders")
+  public String metodoComHeaders(@RequestHeader("name") String name) {
+      return "metodoComHeaders " + name;
+  }
+
+  @PostMapping("/metodoComListHeaders")
+  public String metodoComListHeaders(@RequestHeader Map<String, String> headers) {
+      return "metodoComListHeaders " + headers.entrySet();
+  }
+
+  record Usuario(String username) {}
+}
--- Java/03 - Começando no Spring Boot/01 - Fundamentos do Spring Boot/primeiro-projeto-springboot/src/main/java/com/maciel/primeiro_projeto_springboot/PrimeiroProjetoSpringbootApplication.java
+++ Java/03 - Começando no Spring Boot/01 - Fundamentos do Spring Boot/primeiro-projeto-springboot/src/main/java/com/maciel/primeiro_projeto_springboot/PrimeiroProjetoSpringbootApplication.java
@@ -2,12 +2,13 @@ package com.maciel.primeiro_projeto_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.maciel")
 public class PrimeiroProjetoSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroProjetoSpringbootApplication.class, args);
 	}
-
 }

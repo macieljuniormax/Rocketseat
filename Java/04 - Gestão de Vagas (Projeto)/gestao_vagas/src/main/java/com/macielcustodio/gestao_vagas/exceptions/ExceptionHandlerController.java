27ea72fc21f2543ec38ff836fcf3768364d67f58
Maciel Ferreira Custódio Júnior <maciel_custodio@icloud.com>
2025-08-13 19:40:14 -0300

    feat: :sparkles: create custom exceptions
    - Add `ExceptionHandlerController`
    - Create `ErroMessageDTO` class

---
+++ Java/04 - Gestão de Vagas (Projeto)/gestao_vagas/src/main/java/com/macielcustodio/gestao_vagas/exceptions/ExceptionHandlerController.java
@@ -0,0 +1,32 @@
+package com.macielcustodio.gestao_vagas.exceptions;
+
+import java.util.ArrayList;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+  private MessageSource messageSource;
+
+  public ExceptionHandlerController(MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ArrayList<ErrorMessageDTO>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    ArrayList<ErrorMessageDTO> dto = new ArrayList<>();
+
+    exception.getBindingResult().getFieldErrors().forEach(error -> {
+      String message = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+      dto.add(new ErrorMessageDTO(message, error.getField()));
+    });
+
+    return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+  }
+}

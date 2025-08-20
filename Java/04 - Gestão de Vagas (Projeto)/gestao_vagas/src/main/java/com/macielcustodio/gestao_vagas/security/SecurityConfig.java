c1c176a4caacb107e388fcd387b8d28eaf41d188
Maciel Ferreira Custódio Júnior <maciel_custodio@icloud.com>
2025-08-20 19:32:39 -0300

    feat: :sparkles: add SecurityFilter to handle authorization headers in requests

--- Java/04 - Gestão de Vagas (Projeto)/gestao_vagas/src/main/java/com/macielcustodio/gestao_vagas/security/SecurityConfig.java
+++ Java/04 - Gestão de Vagas (Projeto)/gestao_vagas/src/main/java/com/macielcustodio/gestao_vagas/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.macielcustodio.gestao_vagas.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+  @Autowired
+  private SecurityFilter securityFilter;
+
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     httpSecurity.csrf(csrf -> csrf.disable())
@@ -17,7 +22,7 @@ public class SecurityConfig {
         auth.requestMatchers("/company/", "/auth/company").permitAll();
         auth.anyRequest().authenticated();
       })
-    ;
+      .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
 
     return httpSecurity.build();
   }

51306a6138ba6f205d957ea8e3c9ab07ca91a446
Maciel Ferreira Custódio Júnior <maciel_custodio@icloud.com>
2025-08-20 16:42:05 -0300

    feat: :sparkles: implement PasswordEncoder bean in SecurityConfig and add password encoding in CreateCompanyUseCase and

--- Java/04 - Gestão de Vagas (Projeto)/gestao_vagas/src/main/java/com/macielcustodio/gestao_vagas/modules/company/usecases/CreateCompanyUseCase.java
+++ Java/04 - Gestão de Vagas (Projeto)/gestao_vagas/src/main/java/com/macielcustodio/gestao_vagas/modules/company/usecases/CreateCompanyUseCase.java
@@ -1,6 +1,7 @@
 package com.macielcustodio.gestao_vagas.modules.company.usecases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.macielcustodio.gestao_vagas.exceptions.UserFoundException;
@@ -11,6 +12,8 @@ import com.macielcustodio.gestao_vagas.modules.company.repositories.CompanyRepos
 public class CreateCompanyUseCase {
   @Autowired
   private CompanyRepository companyRepository;
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
   public CompanyEntity execute(CompanyEntity companyEntity) {
     this.companyRepository
@@ -18,6 +21,9 @@ public class CreateCompanyUseCase {
       .ifPresent((_) -> {
         throw new UserFoundException();
       });
+
+    var password = passwordEncoder.encode(companyEntity.getPassword());
+    companyEntity.setPassword(password);
 
     return this.companyRepository.save(companyEntity);
   }

c3bbb6693c89d7e9f22fb9f55aab1d64c1815373
Maciel Ferreira Custódio Júnior <maciel_custodio@icloud.com>
2025-08-24 16:29:53 -0300

    refactor: :recycle: remove unused import in SecurityCandidateFilter

--- Java/04 - Gestão de Vagas (Projeto)/gestao_vagas/src/main/java/com/macielcustodio/gestao_vagas/security/SecurityCandidateFilter.java
+++ Java/04 - Gestão de Vagas (Projeto)/gestao_vagas/src/main/java/com/macielcustodio/gestao_vagas/security/SecurityCandidateFilter.java
@@ -1,7 +1,6 @@
 package com.macielcustodio.gestao_vagas.security;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;

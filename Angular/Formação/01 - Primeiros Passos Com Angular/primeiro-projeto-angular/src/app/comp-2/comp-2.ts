b27a76ed9b7413f10964b1c7f76f74cbde62a0c0
Maciel Custódio <maciel_custodio@icloud.com>
2026-08-13 12:51:34 -0300

    :sparkles: feat: add comp-2 component with HTML, CSS, and test files

---
+++ Angular/Formação/01 - Primeiros Passos Com Angular/primeiro-projeto-angular/src/app/comp-2/comp-2.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-2',
+  imports: [],
+  templateUrl: './comp-2.html',
+  styleUrl: './comp-2.css',
+})
+export class Comp2 {}

23b3459d1a3f787da3efd9ce4e144b62b849d170
Maciel Custódio <maciel_custodio@icloud.com>
2026-08-13 12:51:24 -0300

    :sparkles: feat: add initial component structure with HTML and CSS files

---
+++ Angular/Formação/01 - Primeiros Passos Com Angular/primeiro-projeto-angular/src/app/comp-1/comp-1.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-1',
+  imports: [],
+  templateUrl: './comp-1.html',
+  styleUrl: './comp-1.css',
+})
+export class Comp1 {}
